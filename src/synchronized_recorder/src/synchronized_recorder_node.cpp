c87fdf563749cbf416c79d9771b6588be619599f
Kevin Wu <89735552+KevinWuWorld@users.noreply.github.com>
2025-03-02 17:22:16 -0500

    Update synchronized_recorder_node.cpp
    see 2/10 email for change items

--- src/synchronized_recorder/src/synchronized_recorder_node.cpp
+++ src/synchronized_recorder/src/synchronized_recorder_node.cpp
@@ -16,6 +16,7 @@
 #include <map>
 #include <filesystem> // should be C++17
 
+
 struct KinematicData {
     ros::Time stamp;
     std::vector<double> position;
@@ -23,57 +24,110 @@ struct KinematicData {
     std::vector<double> effort;
 };
 
+
+
 struct ImageData {
     ros::Time stamp;
-    cv::Mat image;
+    cv::Mat   image;
 };
 
-// Shared Data
-std::mutex g_data_mutex;
-std::condition_variable g_cv;
-bool g_data_ready = false;
-bool g_keep_running = true;
 
-std::queue<ImageData> g_image_buffer;
+// Combine the data at an given timestamp into a packet
+struct SyncedPacket {
+    ros::Time        stamp;       // reference timestamp (from the matched data)
+    ImageData        left_img;
+    ImageData        right_img;
+    KinematicData    kin;
+};
+
+
+std::mutex              g_data_mutex;
+std::condition_variable g_cv;
+bool                    g_keep_running = true;
+
+
+// buffers for left image, right image, and kinematics
+std::queue<ImageData>    g_left_image_buffer;
+std::queue<ImageData>    g_right_image_buffer;
 std::queue<KinematicData> g_kinematic_buffer;
 
-ImageData g_synced_image;
-KinematicData g_synced_kinematic;
 
-int g_writers_done = 0;  // How many writers have finished writing the current pair
-std::string g_synced_folder;
 
-const size_t MAX_BUFFER_SIZE = 1000;
-const double TIME_TOL = 0.005;   // 5ms tolerance
+// a queue for matched/synced data that needs to be written
+std::queue<SyncedPacket> g_synced_queue;
 
-void imageCallback(const sensor_msgs::ImageConstPtr &msg) {
+const size_t MAX_BUFFER_SIZE         = 1000;     //
+const size_t MAX_SYNCED_QUEUE_SIZE   = 100;      // queue for writing synced data
+const double TIME_TOL                = 0.005;    // 5ms tolerance
+
+
+// created separate call back functions for left and right stereo camera
+void imageCallbackLeft(const sensor_msgs::ImageConstPtr &msg) {
+
     cv_bridge::CvImageConstPtr cv_ptr;
+
     try {
         cv_ptr = cv_bridge::toCvShare(msg);
     } catch (cv_bridge::Exception &e) {
-        ROS_ERROR("cv_bridge exception: %s", e.what());
+        ROS_ERROR("cv_bridge exception (left): %s", e.what());
         return;
     }
+
 
     ImageData img_data;
     img_data.stamp = msg->header.stamp;
     img_data.image = cv_ptr->image.clone();
 
+
     {
         std::lock_guard<std::mutex> lock(g_data_mutex);
-        if (g_image_buffer.size() > MAX_BUFFER_SIZE) {
-            g_image_buffer.pop();
+        if (g_left_image_buffer.size() > MAX_BUFFER_SIZE) {
+            g_left_image_buffer.pop();
         }
-        g_image_buffer.push(img_data);
+        g_left_image_buffer.push(img_data);
     }
+
+
 }
+
+// created separate call back functions for left and right stereo camera
+void imageCallbackRight(const sensor_msgs::ImageConstPtr &msg) {
+
+    cv_bridge::CvImageConstPtr cv_ptr;
+
+    try {
+        cv_ptr = cv_bridge::toCvShare(msg);
+    } catch (cv_bridge::Exception &e) {
+        ROS_ERROR("cv_bridge exception (right): %s", e.what());
+        return;
+    }
+
+
+    ImageData img_data;
+    img_data.stamp = msg->header.stamp;
+    img_data.image = cv_ptr->image.clone();
+
+
+    {
+        std::lock_guard<std::mutex> lock(g_data_mutex);
+        if (g_right_image_buffer.size() > MAX_BUFFER_SIZE) {
+            g_right_image_buffer.pop();
+        }
+        g_right_image_buffer.push(img_data);
+    }
+
+
+}
+
+
+
 
 void jointStateCallback(const sensor_msgs::JointState::ConstPtr &msg) {
     KinematicData kin_data;
-    kin_data.stamp = msg->header.stamp;
+    kin_data.stamp   = msg->header.stamp;
     kin_data.position = msg->position;
     kin_data.velocity = msg->velocity;
-    kin_data.effort = msg->effort;
+    kin_data.effort   = msg->effort;
 
     {
         std::lock_guard<std::mutex> lock(g_data_mutex);
@@ -84,163 +138,210 @@ void jointStateCallback(const sensor_msgs::JointState::ConstPtr &msg) {
     }
 }
 
-// A helper function to be called by writers after finishing their job
-void writerDone() {
-    std::lock_guard<std::mutex> lock(g_data_mutex);
-    g_writers_done++;
-    if (g_writers_done == 2) {
-        // Both writers have finished successfully
-        g_data_ready = false;
-        g_writers_done = 0;
-    }
-}
-
 // Thread #2: Synchronization
 void syncThread() {
-    std::cout << " ------ entered sync thread ------" << std::endl;
+
+    std::cout << "------ syncThread started ------" << std::endl;
     while (ros::ok() && g_keep_running) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-
         std::lock_guard<std::mutex> lock(g_data_mutex);
-        if (g_data_ready) {
-            // Already have data ready, skip
+
+        // If queue size exceeds threshold, skip data (doesn't happen usually)
+        if (g_synced_queue.size() >= MAX_SYNCED_QUEUE_SIZE) {
+
+
+
+            // drop the oldest data from input buffers
+            
+            if (!g_left_image_buffer.empty()) {
+                g_left_image_buffer.pop();
+            }
+            if (!g_right_image_buffer.empty()) {
+                g_right_image_buffer.pop();
+            }
+            if (!g_kinematic_buffer.empty()) {
+                g_kinematic_buffer.pop();
+            }
+            continue;
+
+        }
+
+
+        if (g_left_image_buffer.empty() || g_right_image_buffer.empty() || g_kinematic_buffer.empty()) {
             continue;
         }
 
-        if (g_image_buffer.empty() || g_kinematic_buffer.empty()) {
-            continue;
-        }
+        
 
-        ImageData img_data = g_image_buffer.front();
+        ImageData left_img  = g_left_image_buffer.front();
+        ImageData right_img = g_right_image_buffer.front();
         KinematicData kin_data = g_kinematic_buffer.front();
-        double time_diff = fabs((img_data.stamp - kin_data.stamp).toSec());
-        if (time_diff < TIME_TOL) {
-            g_synced_image = img_data;
-            g_synced_kinematic = kin_data;
 
-            g_image_buffer.pop();
+        
+        double time_diff_left  = fabs((left_img.stamp  - kin_data.stamp).toSec());
+        double time_diff_right = fabs((right_img.stamp - kin_data.stamp).toSec());
+
+        
+        // check if both left & right images are close in time with the kin data
+        if (time_diff_left < TIME_TOL && time_diff_right < TIME_TOL) {
+
+            // if they match, queue this packet up for writin
+
+            SyncedPacket packet;
+
+            packet.stamp     = kin_data.stamp; 
+            packet.left_img  = left_img;
+            packet.right_img = right_img;
+            packet.kin       = kin_data;
+
+            g_synced_queue.push(packet);
+
+
+
+            g_left_image_buffer.pop();
+            g_right_image_buffer.pop();
             g_kinematic_buffer.pop();
 
-            // Create a folder named after the timestamp of this pair
-            std::ostringstream folder_name;
-            folder_name << "recorded_data/" << g_synced_image.stamp.sec << "_" << g_synced_image.stamp.nsec;
-            g_synced_folder = folder_name.str();
+            // notify the writer thread
+            g_cv.notify_one();
 
-            std::error_code ec;
-            std::filesystem::create_directories(g_synced_folder, ec);
-            if (ec) {
-                std::cerr << "Failed to create directory " << g_synced_folder << ": " << ec.message() << std::endl;
-                g_data_ready = false;
-                continue;
+
+        } else {        // when the time doesn't match...
+
+            
+            ros::Time oldest_stamp = left_img.stamp;
+            if (right_img.stamp < oldest_stamp) {
+                oldest_stamp = right_img.stamp;
+            }
+            if (kin_data.stamp < oldest_stamp) {
+                oldest_stamp = kin_data.stamp;
             }
 
-            g_data_ready = true;
-            g_cv.notify_all();
-        } else {
-            // Discard the older one
-            if (img_data.stamp < kin_data.stamp) {
-                g_image_buffer.pop();
+            if (left_img.stamp == oldest_stamp) {
+                g_left_image_buffer.pop();
+            } else if (right_img.stamp == oldest_stamp) {
+                g_right_image_buffer.pop();
             } else {
                 g_kinematic_buffer.pop();
             }
         }
     }
+
+
+    std::cout << "------ syncThread exiting ------" << std::endl;
+
 }
 
-// Thread #3: Image Writer
-void imageWriterThread() {
-    std::cout << " ------ entered imageWriter thread ------" << std::endl;
+
+
+
+
+
+// Thread #3: Image Writer: combined writing -see packet set-up above
+void writerThread() {
+
+
+    std::cout << "------ writerThread started ------" << std::endl;
+
     while (ros::ok() && g_keep_running) {
         std::unique_lock<std::mutex> lock(g_data_mutex);
-        g_cv.wait(lock, [] { return g_data_ready || !g_keep_running; });
-        if (!g_keep_running) break;
+        // Wait until something is in the queue or we are shutting down
+        g_cv.wait(lock, [] { 
+            return !g_synced_queue.empty() || !g_keep_running; 
+        });
 
-        ImageData local_img = g_synced_image; // Make a copy
-        std::string folder = g_synced_folder;
-        lock.unlock();
-
-        std::string img_filename = folder + "/image.png";
-
-        bool success = cv::imwrite(img_filename, local_img.image);
-
-        // ------ below is an attempt to create separate threads for writing images -------
-        // ------ It doesn't work! Do not enable. Will crash the dvrk console!! -------
-        // std::thread([](std::string filename, cv::Mat img)
-        // {
-        //     cv::imwrite(filename,img);
-        // },
-        // img_filename, local_img.image).detach();
-
-
-        if (success) {
-            std::cout << " ****** wrote a sync image to " << img_filename << " ****** " << std::endl;
-            writerDone(); 
-        } else {
-            std::cerr << "Failed to write image: " << img_filename << std::endl;
-            {
-                std::lock_guard<std::mutex> fail_lock(g_data_mutex);
-                g_data_ready = false;
-                g_writers_done = 0;
-            }
+        if (!g_keep_running && g_synced_queue.empty()) {
+            break; // done
         }
-    }
-}
 
-// Thread #4: Kinematic Writer
-void kinematicWriterThread() {
-    std::cout << " ------ entered kinematicWriter thread ------" << std::endl;
-    while (ros::ok() && g_keep_running) {
-        std::unique_lock<std::mutex> lock(g_data_mutex);
-        g_cv.wait(lock, [] { return g_data_ready || !g_keep_running; });
-        if (!g_keep_running) break;
 
-        KinematicData local_kin = g_synced_kinematic; // Make a copy
-        std::string folder = g_synced_folder;
-        lock.unlock();
 
-        std::string kin_filename = folder + "/kinematics.json";
+        SyncedPacket packet = g_synced_queue.front();
+        g_synced_queue.pop();
 
+        lock.unlock();  
+
+        
+        
+        std::ostringstream folder_name;
+        folder_name << "recorded_data/"
+                    << packet.stamp.sec << "_" 
+                    << packet.stamp.nsec;
+        std::string final_folder = folder_name.str();
+
+        
+        std::error_code ec;
+        std::filesystem::create_directories(final_folder, ec);
+        if (ec) {
+            std::cerr << "Failed to create directory " << final_folder 
+                      << ": " << ec.message() << std::endl;
+            continue;
+        }
+
+
+
+        // Convert from BGR -> RGB as requested
+
+        cv::Mat left_rgb, right_rgb;
+        cv::cvtColor(packet.left_img.image,  left_rgb,  cv::COLOR_BGR2RGB);
+        cv::cvtColor(packet.right_img.image, right_rgb, cv::COLOR_BGR2RGB);
+
+
+
+
+        std::string left_img_path  = final_folder + "/image_left.png";
+        std::string right_img_path = final_folder + "/image_right.png";
+
+
+        bool ok_left  = cv::imwrite(left_img_path,  left_rgb);
+        bool ok_right = cv::imwrite(right_img_path, right_rgb);
+
+
+        if (!ok_left || !ok_right) {
+            std::cerr << "Failed to write images in " << final_folder << std::endl;
+            continue;
+        }
+
+        // below writes kin data
+
+        std::string kin_path = final_folder + "/kinematics.json";
         Json::Value root;
-        root["header"]["sec"] = (Json::Value::Int64)local_kin.stamp.sec;
-        root["header"]["nsec"] = (Json::Value::Int64)local_kin.stamp.nsec;
+        root["header"]["sec"]  = (Json::Value::Int64)packet.kin.stamp.sec;
+        root["header"]["nsec"] = (Json::Value::Int64)packet.kin.stamp.nsec;
 
         Json::Value pos(Json::arrayValue);
-        for (auto &p : local_kin.position) {
-            pos.append(p);
-        }
+        for (auto &p : packet.kin.position) { pos.append(p); }
         root["position"] = pos;
 
         Json::Value vel(Json::arrayValue);
-        for (auto &v : local_kin.velocity) {
-            vel.append(v);
-        }
+        for (auto &v : packet.kin.velocity) { vel.append(v); }
         root["velocity"] = vel;
 
         Json::Value eff(Json::arrayValue);
-        for (auto &e : local_kin.effort) {
-            eff.append(e);
-        }
+        for (auto &e : packet.kin.effort) { eff.append(e); }
         root["effort"] = eff;
 
-        std::ofstream file(kin_filename);
-        if (file) {
-            file << root.toStyledString();
-            file.close();
-            std::cout << " ****** wrote a sync json to " << kin_filename << " ****** " << std::endl;
-            writerDone();
-        } else {
-            std::cerr << "Failed to write json: " << kin_filename << std::endl;
-            {
-                std::lock_guard<std::mutex> fail_lock(g_data_mutex);
-                g_data_ready = false;
-                g_writers_done = 0;
-            }
+        std::ofstream file(kin_path);
+        if (!file) {
+            std::cerr << "Failed to open kinematics.json for writing: " << kin_path << std::endl;
+            continue;
         }
+        file << root.toStyledString();
+        file.close();
+
+        std::cout << "[writerThread] Wrote images + JSON to " << final_folder << std::endl;
+
     }
+
+    std::cout << "------ writerThread exiting ------" << std::endl;
+
 }
 
+
+// ---------------------------- Post-processing functions below ----------------------------
+// 
+ 
 void cleanupFolders() {
-    std::cout << " ------ cleanup step: checking for incomplete folders ------" << std::endl;
+    std::cout << "------ cleanup step: checking for incomplete folders ------" << std::endl;
     std::filesystem::path base_dir("recorded_data");
     if (!std::filesystem::exists(base_dir)) {
         return; // No recorded_data directory
@@ -248,13 +349,16 @@ void cleanupFolders() {
 
     for (auto &entry : std::filesystem::directory_iterator(base_dir)) {
         if (entry.is_directory()) {
-            auto img_path = entry.path() / "image.png";
-            auto json_path = entry.path() / "kinematics.json";
-            bool img_exists = std::filesystem::exists(img_path);
-            bool json_exists = std::filesystem::exists(json_path);
+            auto img_left_path  = entry.path() / "image_left.png";
+            auto img_right_path = entry.path() / "image_right.png";
+            auto json_path      = entry.path() / "kinematics.json";
 
-            // If folder has only JSON and no image, delete the folder
-            if (!img_exists && json_exists) {
+            bool left_exists  = std::filesystem::exists(img_left_path);
+            bool right_exists = std::filesystem::exists(img_right_path);
+            bool json_exists  = std::filesystem::exists(json_path);
+
+            // If the folder does not have all three, remove it
+            if (!left_exists || !right_exists || !json_exists) {
                 std::cout << "Removing incomplete folder: " << entry.path().string() << std::endl;
                 std::filesystem::remove_all(entry.path());
             }
@@ -293,41 +397,102 @@ void countFoldersPerSecond() {
 }
 
 
+void reformatDataStorage() {
+    std::string base_folder = "data_save_folder";
+    std::filesystem::create_directories(base_folder + "/image");
+    std::filesystem::create_directories(base_folder + "/kinematic");
+    std::filesystem::create_directories(base_folder + "/time_syn");
+
+    int index = 0;
+    for (const auto &entry : std::filesystem::directory_iterator("recorded_data")) {
+        if (entry.is_directory()) {
+            std::string img_src = entry.path().string() + "/image.png";
+            std::string kin_src = entry.path().string() + "/kinematics.json";
+            std::string time_syn_src = entry.path().string();
+
+            std::string img_dst = base_folder + "/image/" + std::to_string(index) + ".jpg";
+            std::string kin_dst = base_folder + "/kinematic/" + std::to_string(index) + ".json";
+            std::string time_syn_dst = base_folder + "/time_syn/" + std::to_string(index) + ".json";
+
+            if (std::filesystem::exists(img_src)) {
+                std::filesystem::copy(img_src, img_dst, std::filesystem::copy_options::overwrite_existing);
+            }
+            if (std::filesystem::exists(kin_src)) {
+                std::filesystem::copy(kin_src, kin_dst, std::filesystem::copy_options::overwrite_existing);
+            }
+            
+            Json::Value time_data;
+            time_data["timestamp"] = entry.path().filename().string();
+            std::ofstream file(time_syn_dst);
+            if (file) {
+                file << time_data.toStyledString();
+                file.close();
+            }
+            
+            index++;
+
+
+        }
+    }
+
+
+    // De-comment the line below to remove the recorded_data folder!
+    // The line is commented below for testing purposes
+    // std::filesystem::remove_all("recorded_data");
+}
+
+
+
+
+
+
 int main(int argc, char** argv) {
-    std::cout << " ------ program starting ------" << std::endl;
+    std::cout << "------ program starting ------" << std::endl;
     ros::init(argc, argv, "synchronized_recorder_node");
     ros::NodeHandle nh;
 
     image_transport::ImageTransport it(nh);
-    image_transport::Subscriber image_sub = it.subscribe("/test/right/image_raw", 1, imageCallback);
 
+    image_transport::Subscriber left_sub  = it.subscribe("/test/left/image_raw",  1, imageCallbackLeft);
+    image_transport::Subscriber right_sub = it.subscribe("/test/right/image_raw", 1, imageCallbackRight);
+
+    
     ros::Subscriber joint_sub = nh.subscribe("/PSM1/measured_js", 1, jointStateCallback);
 
-    std::cout << " ------ start ros spin ------" << std::endl;
-    // Spin ROS callback in a separate thread
+
+    std::cout << "------ start ROS spin in separate thread ------" << std::endl;
     std::thread ros_spin_thread([] {
         ros::spin();
+        // Once spin() returns, shutdown
+        std::lock_guard<std::mutex> lk(g_data_mutex);
         g_keep_running = false;
         g_cv.notify_all();
     });
 
-    std::cout << " ------ start threads ------" << std::endl;
+
     std::thread sync_t(syncThread);
-    std::thread img_writer_t(imageWriterThread);
-    std::thread kin_writer_t(kinematicWriterThread);
+
+    std::thread writer_t(writerThread);
 
     ros_spin_thread.join();
-    g_keep_running = false;
-    g_cv.notify_all();
 
+    
+
+    {
+        std::lock_guard<std::mutex> lk(g_data_mutex);
+        g_keep_running = false;
+        g_cv.notify_all();
+    }
+
+    // join threads
     sync_t.join();
-    img_writer_t.join();
-    kin_writer_t.join();
-
-    // Perform cleanup after everything is done
+    writer_t.join();
+ 
     cleanupFolders();
     countFoldersPerSecond();
 
+    
+    reformatDataStorage();
 
     return 0;
 }
