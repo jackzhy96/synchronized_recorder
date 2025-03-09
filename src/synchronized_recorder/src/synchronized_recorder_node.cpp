c40c0ab7965a5de20c69353bca393f6af8b16ba2
Kevin Wu <89735552+KevinWuWorld@users.noreply.github.com>
2025-03-09 14:48:12 -0400

    Update synchronized_recorder_node.cpp
    updated script to include PSM1

--- src/synchronized_recorder/src/synchronized_recorder_node.cpp
+++ src/synchronized_recorder/src/synchronized_recorder_node.cpp
@@ -37,7 +37,8 @@ struct SyncedPacket {
     ros::Time        stamp;       // reference timestamp (from the matched data)
     ImageData        left_img;
     ImageData        right_img;
-    KinematicData    kin;
+    KinematicData    kin_psm1;
+    KinematicData    kin_psm2;
 };
 
 
@@ -50,6 +51,7 @@ bool                    g_keep_running = true;
 std::queue<ImageData>    g_left_image_buffer;
 std::queue<ImageData>    g_right_image_buffer;
 std::queue<KinematicData> g_kinematic_buffer;
+std::queue<KinematicData> g_kinematic_buffer_psm2;
 
 
 
@@ -120,8 +122,6 @@ void imageCallbackRight(const sensor_msgs::ImageConstPtr &msg) {
 }
 
 
-
-
 void jointStateCallback(const sensor_msgs::JointState::ConstPtr &msg) {
     KinematicData kin_data;
     kin_data.stamp   = msg->header.stamp;
@@ -129,148 +129,178 @@ void jointStateCallback(const sensor_msgs::JointState::ConstPtr &msg) {
     kin_data.velocity = msg->velocity;
     kin_data.effort   = msg->effort;
 
+    
     {
         std::lock_guard<std::mutex> lock(g_data_mutex);
         if (g_kinematic_buffer.size() > MAX_BUFFER_SIZE) {
             g_kinematic_buffer.pop();
         }
+
         g_kinematic_buffer.push(kin_data);
     }
+
+
 }
+
+// note: the function below is NOT a duplicate of the function above! The function below handles PSM2. 
+void jointStateCallbackPSM2(const sensor_msgs::JointState::ConstPtr &msg) {
+    KinematicData kin_data;
+    kin_data.stamp    = msg->header.stamp;
+    kin_data.position = msg->position;
+    kin_data.velocity = msg->velocity;
+    kin_data.effort   = msg->effort;
+
+
+    {
+        std::lock_guard<std::mutex> lock(g_data_mutex);
+        if (g_kinematic_buffer_psm2.size() > MAX_BUFFER_SIZE) {
+            g_kinematic_buffer_psm2.pop();
+        }
+        
+        g_kinematic_buffer_psm2.push(kin_data);
+    }
+
+
+}
+
 
 // Thread #2: Synchronization
 void syncThread() {
 
     std::cout << "------ syncThread started ------" << std::endl;
+    
     while (ros::ok() && g_keep_running) {
+
         std::lock_guard<std::mutex> lock(g_data_mutex);
 
-        // If queue size exceeds threshold, skip data (doesn't happen usually)
+
         if (g_synced_queue.size() >= MAX_SYNCED_QUEUE_SIZE) {
-
-
-
             // drop the oldest data from input buffers
-            
-            if (!g_left_image_buffer.empty()) {
-                g_left_image_buffer.pop();
-            }
-            if (!g_right_image_buffer.empty()) {
-                g_right_image_buffer.pop();
-            }
-            if (!g_kinematic_buffer.empty()) {
-                g_kinematic_buffer.pop();
-            }
+
+            if (!g_left_image_buffer.empty())    g_left_image_buffer.pop();
+            if (!g_right_image_buffer.empty())   g_right_image_buffer.pop();
+            if (!g_kinematic_buffer.empty())     g_kinematic_buffer.pop();
+            if (!g_kinematic_buffer_psm2.empty()) g_kinematic_buffer_psm2.pop();
             continue;
+
 
         }
 
-
-        if (g_left_image_buffer.empty() || g_right_image_buffer.empty() || g_kinematic_buffer.empty()) {
+        // if any buffer is empty, can't sync
+        if (g_left_image_buffer.empty() ||
+            g_right_image_buffer.empty() ||
+            g_kinematic_buffer.empty() ||            // PSM1
+            g_kinematic_buffer_psm2.empty()) {       // PSM2
             continue;
         }
 
         
+        ImageData     left_img  = g_left_image_buffer.front();
+        ImageData     right_img = g_right_image_buffer.front();
+        KinematicData kin1      = g_kinematic_buffer.front();       // PSM1
+        KinematicData kin2      = g_kinematic_buffer_psm2.front();  // PSM2
 
-        ImageData left_img  = g_left_image_buffer.front();
-        ImageData right_img = g_right_image_buffer.front();
-        KinematicData kin_data = g_kinematic_buffer.front();
-
-        
-        double time_diff_left  = fabs((left_img.stamp  - kin_data.stamp).toSec());
-        double time_diff_right = fabs((right_img.stamp - kin_data.stamp).toSec());
 
         
-        // check if both left & right images are close in time with the kin data
-        if (time_diff_left < TIME_TOL && time_diff_right < TIME_TOL) {
+        double time_diff_left  = fabs((left_img.stamp   - kin1.stamp).toSec());
+        double time_diff_right = fabs((right_img.stamp  - kin1.stamp).toSec());
+        double time_diff_kin2  = fabs((kin2.stamp       - kin1.stamp).toSec());
 
-            // if they match, queue this packet up for writin
+
+
+        if (time_diff_left < TIME_TOL &&
+            time_diff_right < TIME_TOL &&
+            time_diff_kin2 < TIME_TOL) 
+        {
+
 
             SyncedPacket packet;
 
-            packet.stamp     = kin_data.stamp; 
+            packet.stamp     = kin1.stamp; 
             packet.left_img  = left_img;
             packet.right_img = right_img;
-            packet.kin       = kin_data;
+            packet.kin_psm1  = kin1;
+            packet.kin_psm2  = kin2;
+
 
             g_synced_queue.push(packet);
 
 
-
+            // pop them all
             g_left_image_buffer.pop();
             g_right_image_buffer.pop();
-            g_kinematic_buffer.pop();
+            g_kinematic_buffer.pop();       // PSM1
+            g_kinematic_buffer_psm2.pop();  // PSM2
 
-            // notify the writer thread
             g_cv.notify_one();
-
-
-        } else {        // when the time doesn't match...
-
+        } 
+        
+        
+        else {
             
             ros::Time oldest_stamp = left_img.stamp;
-            if (right_img.stamp < oldest_stamp) {
-                oldest_stamp = right_img.stamp;
-            }
-            if (kin_data.stamp < oldest_stamp) {
-                oldest_stamp = kin_data.stamp;
-            }
+            if (right_img.stamp < oldest_stamp) oldest_stamp = right_img.stamp;
+            if (kin1.stamp < oldest_stamp)      oldest_stamp = kin1.stamp;
+            if (kin2.stamp < oldest_stamp)      oldest_stamp = kin2.stamp;
 
             if (left_img.stamp == oldest_stamp) {
                 g_left_image_buffer.pop();
-            } else if (right_img.stamp == oldest_stamp) {
+            } 
+
+            else if (right_img.stamp == oldest_stamp) {
                 g_right_image_buffer.pop();
-            } else {
-                g_kinematic_buffer.pop();
+            } 
+            else if (kin1.stamp == oldest_stamp) {
+                g_kinematic_buffer.pop();        // PSM1
+            } 
+            else {
+                g_kinematic_buffer_psm2.pop();   // PSM2
             }
+
+            
         }
+
+
     }
-
-
     std::cout << "------ syncThread exiting ------" << std::endl;
-
 }
-
-
 
 
 
 
 // Thread #3: Image Writer: combined writing -see packet set-up above
 void writerThread() {
-
-
     std::cout << "------ writerThread started ------" << std::endl;
+
 
     while (ros::ok() && g_keep_running) {
         std::unique_lock<std::mutex> lock(g_data_mutex);
-        // Wait until something is in the queue or we are shutting down
-        g_cv.wait(lock, [] { 
-            return !g_synced_queue.empty() || !g_keep_running; 
+        g_cv.wait(lock, [] {
+            return !g_synced_queue.empty() || !g_keep_running;
         });
 
-        if (!g_keep_running && g_synced_queue.empty()) {
-            break; // done
-        }
 
+        if (!g_keep_running && g_synced_queue.empty()) {
+            break;
+        }
 
 
         SyncedPacket packet = g_synced_queue.front();
         g_synced_queue.pop();
+        lock.unlock();
 
-        lock.unlock();  
 
-        
-        
         std::ostringstream folder_name;
         folder_name << "recorded_data/"
-                    << packet.stamp.sec << "_" 
+                    << packet.stamp.sec << "_"
                     << packet.stamp.nsec;
         std::string final_folder = folder_name.str();
 
-        
+
         std::error_code ec;
         std::filesystem::create_directories(final_folder, ec);
+
+
         if (ec) {
             std::cerr << "Failed to create directory " << final_folder 
                       << ": " << ec.message() << std::endl;
@@ -280,18 +310,14 @@ void writerThread() {
 
 
         // Convert from BGR -> RGB as requested
-
         cv::Mat left_rgb, right_rgb;
         cv::cvtColor(packet.left_img.image,  left_rgb,  cv::COLOR_BGR2RGB);
         cv::cvtColor(packet.right_img.image, right_rgb, cv::COLOR_BGR2RGB);
 
 
 
-
         std::string left_img_path  = final_folder + "/image_left.png";
         std::string right_img_path = final_folder + "/image_right.png";
-
-
         bool ok_left  = cv::imwrite(left_img_path,  left_rgb);
         bool ok_right = cv::imwrite(right_img_path, right_rgb);
 
@@ -301,70 +327,131 @@ void writerThread() {
             continue;
         }
 
-        // below writes kin data
 
-        std::string kin_path = final_folder + "/kinematics.json";
-        Json::Value root;
-        root["header"]["sec"]  = (Json::Value::Int64)packet.kin.stamp.sec;
-        root["header"]["nsec"] = (Json::Value::Int64)packet.kin.stamp.nsec;
 
-        Json::Value pos(Json::arrayValue);
-        for (auto &p : packet.kin.position) { pos.append(p); }
-        root["position"] = pos;
 
-        Json::Value vel(Json::arrayValue);
-        for (auto &v : packet.kin.velocity) { vel.append(v); }
-        root["velocity"] = vel;
+        // --- Write PSM1 kinematics ---
+        {
 
-        Json::Value eff(Json::arrayValue);
-        for (auto &e : packet.kin.effort) { eff.append(e); }
-        root["effort"] = eff;
+            std::string kin_path_psm1 = final_folder + "/kinematics_PSM1.json";
+            Json::Value root;
+            root["header"]["sec"]  = (Json::Value::Int64)packet.kin_psm1.stamp.sec;
+            root["header"]["nsec"] = (Json::Value::Int64)packet.kin_psm1.stamp.nsec;
 
-        std::ofstream file(kin_path);
-        if (!file) {
-            std::cerr << "Failed to open kinematics.json for writing: " << kin_path << std::endl;
-            continue;
+
+            Json::Value pos(Json::arrayValue);
+            for (auto &p : packet.kin_psm1.position) { pos.append(p); }
+            root["position"] = pos;
+
+
+            Json::Value vel(Json::arrayValue);
+            for (auto &v : packet.kin_psm1.velocity) { vel.append(v); }
+            root["velocity"] = vel;
+
+
+            Json::Value eff(Json::arrayValue);
+            for (auto &e : packet.kin_psm1.effort) { eff.append(e); }
+            root["effort"] = eff;
+
+
+            std::ofstream file(kin_path_psm1);
+            if (!file) {
+                std::cerr << "Failed to open kinematics_PSM1.json for writing.\n";
+                continue;
+            }
+
+
+            file << root.toStyledString();
+            file.close();
         }
-        file << root.toStyledString();
-        file.close();
 
-        std::cout << "[writerThread] Wrote images + JSON to " << final_folder << std::endl;
+        // --- Write PSM2 kinematics ---
+        {
 
+            std::string kin_path_psm2 = final_folder + "/kinematics_PSM2.json";
+            Json::Value root;
+            root["header"]["sec"]  = (Json::Value::Int64)packet.kin_psm2.stamp.sec;
+            root["header"]["nsec"] = (Json::Value::Int64)packet.kin_psm2.stamp.nsec;
+
+
+            Json::Value pos(Json::arrayValue);
+            for (auto &p : packet.kin_psm2.position) { pos.append(p); }
+            root["position"] = pos;
+
+
+            Json::Value vel(Json::arrayValue);
+            for (auto &v : packet.kin_psm2.velocity) { vel.append(v); }
+            root["velocity"] = vel;
+
+
+            Json::Value eff(Json::arrayValue);
+            for (auto &e : packet.kin_psm2.effort) { eff.append(e); }
+            root["effort"] = eff;
+
+
+            std::ofstream file(kin_path_psm2);
+            if (!file) {
+                std::cerr << "Failed to open kinematics_PSM2.json for writing.\n";
+                continue;
+            }
+
+            file << root.toStyledString();
+            file.close();
+        }
+
+
+        std::cout << "[writerThread] Wrote images + PSM1 & PSM2 JSON to " 
+                  << final_folder << std::endl;
     }
 
-    std::cout << "------ writerThread exiting ------" << std::endl;
 
+    std::cout << "------ writerThread exiting ------" << std::endl;
 }
 
 
+
+
+
 // ---------------------------- Post-processing functions below ----------------------------
-// 
- 
+
+
 void cleanupFolders() {
     std::cout << "------ cleanup step: checking for incomplete folders ------" << std::endl;
     std::filesystem::path base_dir("recorded_data");
+
     if (!std::filesystem::exists(base_dir)) {
-        return; // No recorded_data directory
+        return;
     }
 
     for (auto &entry : std::filesystem::directory_iterator(base_dir)) {
+
         if (entry.is_directory()) {
+
             auto img_left_path  = entry.path() / "image_left.png";
             auto img_right_path = entry.path() / "image_right.png";
-            auto json_path      = entry.path() / "kinematics.json";
+            auto json_psm1_path = entry.path() / "kinematics_PSM1.json";
+            auto json_psm2_path = entry.path() / "kinematics_PSM2.json";
 
-            bool left_exists  = std::filesystem::exists(img_left_path);
-            bool right_exists = std::filesystem::exists(img_right_path);
-            bool json_exists  = std::filesystem::exists(json_path);
 
-            // If the folder does not have all three, remove it
-            if (!left_exists || !right_exists || !json_exists) {
-                std::cout << "Removing incomplete folder: " << entry.path().string() << std::endl;
+            bool left_exists   = std::filesystem::exists(img_left_path);
+            bool right_exists  = std::filesystem::exists(img_right_path);
+            bool psm1_exists   = std::filesystem::exists(json_psm1_path);
+            bool psm2_exists   = std::filesystem::exists(json_psm2_path);
+
+
+
+
+            // If missing any of the 4, remove the folder
+            if (!left_exists || !right_exists || !psm1_exists || !psm2_exists) {
+                std::cout << "Removing incomplete folder: " 
+                          << entry.path().string() << std::endl;
                 std::filesystem::remove_all(entry.path());
             }
         }
     }
 }
+
+
 
 void countFoldersPerSecond() {
     std::cout << " ------ Gathering output frequency during runtime ------" << std::endl;
@@ -397,6 +484,8 @@ void countFoldersPerSecond() {
 }
 
 
+
+
 void reformatDataStorage() {
     std::string base_folder = "data_save_folder";
     std::filesystem::create_directories(base_folder + "/image");
@@ -406,26 +495,38 @@ void reformatDataStorage() {
     int index = 0;
     for (const auto &entry : std::filesystem::directory_iterator("recorded_data")) {
         if (entry.is_directory()) {
-            std::string img_left_src = entry.path().string() + "/image_left.png";
-            std::string img_right_src = entry.path().string() + "/image_right.png";
-            std::string kin_src = entry.path().string() + "/kinematics.json";
-            std::string time_syn_src = entry.path().string();
+            std::string img_left_src   = entry.path().string() + "/image_left.png";
+            std::string img_right_src  = entry.path().string() + "/image_right.png";
+            std::string kin_src_psm1   = entry.path().string() + "/kinematics_PSM1.json";
+            std::string kin_src_psm2   = entry.path().string() + "/kinematics_PSM2.json";
+            std::string time_syn_src   = entry.path().string(); // the entire folder as context
 
-            std::string img_left_dst = base_folder + "/image/" + std::to_string(index) + "_left.png";
-            std::string img_right_dst = base_folder + "/image/" + std::to_string(index) + "_right.png";
-            std::string kin_dst = base_folder + "/kinematic/" + std::to_string(index) + ".json";
-            std::string time_syn_dst = base_folder + "/time_syn/" + std::to_string(index) + ".json";
+            std::string img_left_dst   = base_folder + "/image/" + std::to_string(index) + "_left.png";
+            std::string img_right_dst  = base_folder + "/image/" + std::to_string(index) + "_right.png";
+            // Now name the kinematics files "index_PSM1.json" and "index_PSM2.json"
+            std::string kin_dst_psm1   = base_folder + "/kinematic/" + std::to_string(index) + "_PSM1.json";
+            std::string kin_dst_psm2   = base_folder + "/kinematic/" + std::to_string(index) + "_PSM2.json";
+
+            std::string time_syn_dst   = base_folder + "/time_syn/" + std::to_string(index) + ".json";
 
             if (std::filesystem::exists(img_left_src)) {
-                std::filesystem::copy(img_left_src, img_left_dst, std::filesystem::copy_options::overwrite_existing);
+                std::filesystem::copy(img_left_src, img_left_dst,
+                                      std::filesystem::copy_options::overwrite_existing);
             }
             if (std::filesystem::exists(img_right_src)) {
-                std::filesystem::copy(img_right_src, img_right_dst, std::filesystem::copy_options::overwrite_existing);
+                std::filesystem::copy(img_right_src, img_right_dst,
+                                      std::filesystem::copy_options::overwrite_existing);
             }
-            if (std::filesystem::exists(kin_src)) {
-                std::filesystem::copy(kin_src, kin_dst, std::filesystem::copy_options::overwrite_existing);
+            if (std::filesystem::exists(kin_src_psm1)) {
+                std::filesystem::copy(kin_src_psm1, kin_dst_psm1,
+                                      std::filesystem::copy_options::overwrite_existing);
             }
-            
+            if (std::filesystem::exists(kin_src_psm2)) {
+                std::filesystem::copy(kin_src_psm2, kin_dst_psm2,
+                                      std::filesystem::copy_options::overwrite_existing);
+            }
+
+            // Save time info
             Json::Value time_data;
             time_data["timestamp"] = entry.path().filename().string();
             std::ofstream file(time_syn_dst);
@@ -433,20 +534,15 @@ void reformatDataStorage() {
                 file << time_data.toStyledString();
                 file.close();
             }
-            
+
             index++;
-
-
         }
     }
-
 
     // De-comment the line below to remove the recorded_data folder!
     // The line is commented below for testing purposes
     // std::filesystem::remove_all("recorded_data");
 }
-
-
 
 
 
@@ -461,8 +557,12 @@ int main(int argc, char** argv) {
     image_transport::Subscriber left_sub  = it.subscribe("/test/left/image_raw",  1, imageCallbackLeft);
     image_transport::Subscriber right_sub = it.subscribe("/test/right/image_raw", 1, imageCallbackRight);
 
-    
-    ros::Subscriber joint_sub = nh.subscribe("/PSM1/measured_js", 1, jointStateCallback);
+
+    ros::Subscriber joint_sub_psm1 = nh.subscribe("/PSM1/measured_js", 1, jointStateCallback);
+
+    // adding in a subscriber for PSM2
+    ros::Subscriber joint_sub_psm2 = nh.subscribe("/PSM2/measured_js", 1, jointStateCallbackPSM2);
+
 
 
     std::cout << "------ start ROS spin in separate thread ------" << std::endl;
